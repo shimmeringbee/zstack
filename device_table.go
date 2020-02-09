a269a80e0da0af6581ca91f45c480c0b474a7a97
Peter Wood <peter@alastria.net>
2020-02-09 21:59:42 +0000

    Add new device table to replace the existing device cache.

---
+++ device_table.go
@@ -0,0 +1,116 @@
+package zstack
+
+import (
+	"github.com/shimmeringbee/zigbee"
+	"time"
+)
+
+type DeviceTable struct {
+	ieeeToDevice  map[zigbee.IEEEAddress]*Device
+	networkToIEEE map[zigbee.NetworkAddress]zigbee.IEEEAddress
+}
+
+func NewDeviceTable() *DeviceTable {
+	return &DeviceTable{
+		ieeeToDevice:  make(map[zigbee.IEEEAddress]*Device),
+		networkToIEEE: make(map[zigbee.NetworkAddress]zigbee.IEEEAddress),
+	}
+}
+
+func (t *DeviceTable) GetByIEEE(ieeeAddress zigbee.IEEEAddress) (Device, bool) {
+	device, found := t.ieeeToDevice[ieeeAddress]
+
+	if found {
+		return *device, found
+	} else {
+		return Device{}, false
+	}
+}
+
+func (t *DeviceTable) GetByNetwork(networkAddress zigbee.NetworkAddress) (Device, bool) {
+	ieee, found := t.networkToIEEE[networkAddress]
+
+	if !found {
+		return Device{}, false
+	} else {
+		return t.GetByIEEE(ieee)
+	}
+}
+
+func (t *DeviceTable) AddOrUpdate(ieeeAddress zigbee.IEEEAddress, networkAddress zigbee.NetworkAddress, updates ...DeviceUpdate) {
+	device, found := t.ieeeToDevice[ieeeAddress]
+
+	if found {
+		delete(t.networkToIEEE, device.NetworkAddress)
+		device.NetworkAddress = networkAddress
+	} else {
+		t.ieeeToDevice[ieeeAddress] = &Device{
+			IEEEAddress:    ieeeAddress,
+			NetworkAddress: networkAddress,
+		}
+	}
+
+	t.networkToIEEE[networkAddress] = ieeeAddress
+	t.Update(ieeeAddress, updates...)
+}
+
+func (t *DeviceTable) Update(ieeeAddress zigbee.IEEEAddress, updates ...DeviceUpdate) {
+	device, found := t.ieeeToDevice[ieeeAddress]
+
+	if found {
+		for _, du := range updates {
+			du(device)
+		}
+	}
+}
+
+func (t *DeviceTable) Remove(ieeeAddress zigbee.IEEEAddress) {
+	device, found := t.GetByIEEE(ieeeAddress)
+
+	if found {
+		delete(t.networkToIEEE, device.NetworkAddress)
+		delete(t.ieeeToDevice, device.IEEEAddress)
+	}
+}
+
+type DeviceUpdate func(device *Device)
+
+func LogicalType(logicalType zigbee.LogicalType) DeviceUpdate {
+	return func(device *Device) {
+		if logicalType > zigbee.EndDevice {
+			return
+		}
+
+		device.LogicalType = logicalType
+	}
+}
+
+func LQI(lqi uint8) DeviceUpdate {
+	return func(device *Device) {
+		device.LQI = lqi
+	}
+}
+
+func Depth(depth uint8) DeviceUpdate {
+	return func(device *Device) {
+		device.Depth = depth
+	}
+}
+
+func UpdateReceived(device *Device) {
+	device.LastReceived = time.Now()
+}
+
+func UpdateDiscovered(device *Device) {
+	device.LastDiscovered = time.Now()
+}
+
+type Device struct {
+	IEEEAddress    zigbee.IEEEAddress
+	NetworkAddress zigbee.NetworkAddress
+	LogicalType    zigbee.LogicalType
+	LQI            uint8
+	Depth          uint8
+	LastDiscovered time.Time
+	LastReceived   time.Time
+}
---
+++ device_table_test.go
@@ -0,0 +1,158 @@
+package zstack
+
+import (
+	"github.com/shimmeringbee/zigbee"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestDeviceTable(t *testing.T) {
+	ieee := zigbee.IEEEAddress(0x001122334455)
+	network := zigbee.NetworkAddress(0xaabb)
+
+	t.Run("an added device can be retrieved by its IEEE address, and has minimum information", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		dt.AddOrUpdate(ieee, network)
+
+		device, found := dt.GetByIEEE(ieee)
+
+		assert.True(t, found)
+		assert.Equal(t, ieee, device.IEEEAddress)
+		assert.Equal(t, network, device.NetworkAddress)
+	})
+
+	t.Run("an added device with updates can be retrieved and has updated information", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		dt.AddOrUpdate(ieee, network, LogicalType(zigbee.EndDevice))
+
+		device, found := dt.GetByIEEE(ieee)
+
+		assert.True(t, found)
+		assert.Equal(t, zigbee.EndDevice, device.LogicalType)
+	})
+
+	t.Run("an added device can be retrieved by its network address", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		dt.AddOrUpdate(ieee, network)
+
+		_, found := dt.GetByNetwork(network)
+		assert.True(t, found)
+	})
+
+	t.Run("a missing device queried by its ieee address returns not found", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		_, found := dt.GetByIEEE(ieee)
+		assert.False(t, found)
+	})
+
+	t.Run("a missing device queried by its network address returns not found", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		_, found := dt.GetByNetwork(network)
+		assert.False(t, found)
+	})
+
+	t.Run("removing a device results in it not being found by ieee address", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		dt.AddOrUpdate(ieee, network)
+		dt.Remove(ieee)
+
+		_, found := dt.GetByIEEE(ieee)
+		assert.False(t, found)
+	})
+
+	t.Run("removing a device results in it not being found by network address", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		dt.AddOrUpdate(ieee, network)
+		dt.Remove(ieee)
+
+		_, found := dt.GetByNetwork(network)
+		assert.False(t, found)
+	})
+
+	t.Run("an update using add makes the device available under the new network only, and updates the network address", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		newNetwork := zigbee.NetworkAddress(0x1234)
+
+		dt.AddOrUpdate(ieee, network)
+		dt.AddOrUpdate(ieee, newNetwork)
+
+		_, found := dt.GetByNetwork(network)
+		assert.False(t, found)
+
+		device, found := dt.GetByNetwork(newNetwork)
+		assert.True(t, found)
+
+		assert.Equal(t, newNetwork, device.NetworkAddress)
+	})
+
+	t.Run("an update makes all changes as requested by device updates", func(t *testing.T) {
+		dt := NewDeviceTable()
+
+		dt.AddOrUpdate(ieee, network)
+
+		dt.Update(ieee, LogicalType(zigbee.EndDevice))
+
+		d, _ := dt.GetByIEEE(ieee)
+
+		assert.Equal(t, zigbee.EndDevice, d.LogicalType)
+	})
+}
+
+func TestDeviceUpdate(t *testing.T) {
+	t.Run("LogicalType updates the logical type of device", func(t *testing.T) {
+		device := &Device{}
+
+		LogicalType(zigbee.EndDevice)(device)
+
+		assert.Equal(t, zigbee.EndDevice, device.LogicalType)
+	})
+
+	t.Run("LogicalType does not update the logical type of device if it is unknown", func(t *testing.T) {
+		device := &Device{LogicalType:zigbee.Router}
+
+		LogicalType(zigbee.Unknown)(device)
+
+		assert.Equal(t, zigbee.Router, device.LogicalType)
+	})
+
+	t.Run("LQI updates the lqi of device", func(t *testing.T) {
+		device := &Device{}
+
+		LQI(48)(device)
+
+		assert.Equal(t, uint8(48), device.LQI)
+	})
+
+	t.Run("Depth updates the depth of device", func(t *testing.T) {
+		device := &Device{}
+
+		Depth(3)(device)
+
+		assert.Equal(t, uint8(3), device.Depth)
+	})
+
+	t.Run("UpdateReceived updates the last received time of device", func(t *testing.T) {
+		device := &Device{}
+
+		UpdateReceived(device)
+
+		assert.NotEqual(t, time.Time{}, device.LastReceived)
+	})
+
+	t.Run("UpdateDiscovered updates the last received time of device", func(t *testing.T) {
+		device := &Device{}
+
+		UpdateDiscovered(device)
+
+		assert.NotEqual(t, time.Time{}, device.LastDiscovered)
+	})
+}
